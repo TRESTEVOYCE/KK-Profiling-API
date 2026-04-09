bbb52433a7d6856144ff793c3334f108105fd762
mark <hitsugayasubaru@gmail.com>
2026-04-09 07:37:28 -0700

    added authetication

--- sk_members_api/views.py
+++ sk_members_api/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
 from sk_members_api.serializers import MemberSerializer
+from rest_framework.permissions import IsAuthenticated
 from .models import Member
 # Create your views here.
 
 
 class MemberListCreate(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
 class MemberRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
