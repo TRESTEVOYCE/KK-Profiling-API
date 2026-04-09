19ef32e89a6ebf533f79328144f6f24787ce9ba9
mark <hitsugayasubaru@gmail.com>
2026-04-09 06:33:07 -0700

    configured the API for sk members

--- sk_members_api/views.py
+++ sk_members_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from sk_members_api.serializers import MemberSerializer
+from .models import Member
 # Create your views here.
+
+
+class MemberViewSet(viewsets.ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+
